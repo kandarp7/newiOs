9ca3427c910a438ed2afc85f64aa8e3be5e2edad
Kandarp Dave <kandarp7@hotmail.com>
2014-05-12 20:33:22 -0400

    Initial Commit

---
+++ Emptyproj/UICollectionAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UICollectionAppDelegate.h
+//  Emptyproj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Emptyproj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Emptyproj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UICollectionAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UICollectionAppDelegate class]));
+    }
+}
